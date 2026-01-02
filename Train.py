fa0cfcd9b857a5c553ad677c4f2817d0b7376276
ByteTrailsHub <bytetrail24@gmail.com>
2026-01-02 19:33:56 +0700

    Make file paths portable with pathlib

--- Train.py
+++ Train.py
@@ -1,9 +1,13 @@
 import pandas as pd
-import sqlite3
+from pathlib import Path
 import matplotlib.pyplot as plt
 
+BASE_DIR = Path(__file__).resolve().parent
+DATA_DIR = BASE_DIR / "data"
+OUT_DIR = BASE_DIR / "output"
+OUT_DIR.mkdir(exist_ok=True)
 
-df = pd.read_csv("data/data_sales.csv")
+df = pd.read_csv(DATA_DIR / "data_sales.csv")
 df=df.drop(index=0)
 df['revenue'] = df['units_sold'] * df['price']
 df['gross_profit'] = (df['price'] - df['cost']) * df['units_sold']
@@ -25,26 +29,26 @@ category_check = (
 category_check["total_gp_percent"] = (category_check["total_gross_profit"]/category_check["total_revenue"]) *100
 
 daily_summary.plot(kind='line', x='date', y='total_revenue')
-plt.savefig(r'C:\Users\ebeld\PycharmProjects\Python_training\output\daily_revenue_trend.png', bbox_inches='tight')
+plt.savefig(OUT_DIR / "daily_revenue_trend.png", bbox_inches="tight")
 plt.close()
-plt.show()
+
 
 sorted_check = category_check.sort_values(by='total_revenue', ascending=False)
 sorted_check.plot(kind='bar', x='category', y='total_revenue')
-plt.savefig(r'C:\Users\ebeld\PycharmProjects\Python_training\output\category_revenue.png', bbox_inches='tight')
+plt.savefig(OUT_DIR / "category_revenue.png", bbox_inches="tight")
 plt.close()
 
 sorted_check2 = category_check.sort_values(by='total_gp_percent', ascending=False)
 sorted_check2.plot(kind='bar', x='category', y='total_gp_percent')
-plt.savefig(r'C:\Users\ebeld\PycharmProjects\Python_training\output\category_gp_percent.png', bbox_inches='tight')
+plt.savefig(OUT_DIR / "category_gp_percent.png", bbox_inches="tight")
 plt.close()
 
-#plt.show()
+
 plt.close()
 #print(df.head())
 # print(daily_summary.head())
 #print(category_check.head())
 #print(sorted_check2.head())
 
-daily_summary.to_csv(r'C:\Users\ebeld\PycharmProjects\Python_training\data\daily_revenue_summary.csv')
-sorted_check.to_csv(r'C:\Users\ebeld\PycharmProjects\Python_training\data\category_performance_summary.csv')
+daily_summary.to_csv(OUT_DIR /'daily_revenue_summary.csv', index=False)
+sorted_check.to_csv(OUT_DIR /'category_performance_summary.csv', index=False)
